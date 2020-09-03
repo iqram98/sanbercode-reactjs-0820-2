3807906e793d713dd82cf401f8b74dfb7a9fa2cb
Iqram Nugraha <iqram7898@gmail.com>
2020-09-03 21:49:55 +0700

    Membuat tugas 9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form Pembelian Buah</h1>
+      <table>
+        <tr>
+          <td>
+            <b>Nama Pelanggan</b>
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <b>Daftar Item</b>
+          </td>
+          <td>
+            <input type="checkbox" />
+            Semangka <br />
+            <input type="checkbox" />
+            Jeruk <br />
+            <input type="checkbox" />
+            Nanas <br />
+            <input type="checkbox" />
+            Salak <br />
+            <input type="checkbox" />
+            Anggur <br />
+          </td>
+        </tr>
+      </table>
+      <button>Krim</button>
     </div>
   );
 }
