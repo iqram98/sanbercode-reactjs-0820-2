7b636a509fca64121bd66089e5ef01109a9c40c9
Iqram Nugraha <iqram7898@gmail.com>
2020-09-04 14:04:18 +0700

    Membuat Tugas 10

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import FormBuah from "./tugas-9/FormBuah";
+import TabelHargaBuah from "./tugas-10/TabelHargaBuah";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Form Pembelian Buah</h1>
-      <table>
-        <tr>
-          <td>
-            <b>Nama Pelanggan</b>
-          </td>
-          <td>
-            <input type="text" />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <b>Daftar Item</b>
-          </td>
-          <td>
-            <input type="checkbox" />
-            Semangka <br />
-            <input type="checkbox" />
-            Jeruk <br />
-            <input type="checkbox" />
-            Nanas <br />
-            <input type="checkbox" />
-            Salak <br />
-            <input type="checkbox" />
-            Anggur <br />
-          </td>
-        </tr>
-      </table>
-      <button>Krim</button>
+      <FormBuah />
+      <TabelHargaBuah />
     </div>
   );
 }
