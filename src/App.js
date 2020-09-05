c0ea981d9aeb94206498e074e81162a57caadf0f
Iqram Nugraha <iqram7898@gmail.com>
2020-09-05 10:31:38 +0700

    Update tugas 10

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import FormBuah from "./tugas-9/FormBuah";
 import TabelHargaBuah from "./tugas-10/TabelHargaBuah";
 import "./App.css";
 
+let dataHargaBuah = [
+  { nama: "Semangka", harga: 10000, berat: 1000 },
+  { nama: "Anggur", harga: 40000, berat: 500 },
+  { nama: "Strawberry", harga: 30000, berat: 400 },
+  { nama: "Jeruk", harga: 30000, berat: 1000 },
+  { nama: "Mangga", harga: 30000, berat: 500 },
+];
+
 function App() {
   return (
     <div className="App">
       <FormBuah />
-      <TabelHargaBuah />
+      <TabelHargaBuah harga={dataHargaBuah} />
     </div>
   );
 }
